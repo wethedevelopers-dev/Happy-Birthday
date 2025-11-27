5687bbcc41aee9b81255d51542b502418a61dd3c
TheBitForge <wethedevelopers.dev@gmail.com>
2025-11-27 14:25:03 +0500

    Change countdown date and implement music play
    Updated countdown date and added music play functionality.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const second = 1000,
   minute = second * 60,
   hour = minute * 60,
   day = hour * 24;
-let countDown = new Date('Oct 22, 2023 00:00:00').getTime(),
+let countDown = new Date('Dec 01, 2025 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -446,3 +446,4 @@ function confetti() {
 
   if (!onlyOnKonami) poof();
 };
+
